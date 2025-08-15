f884dcdd7e431c788520dfd1910e3e06a6678f1c
Arthur Henrique da Silva Blaudt <odairblaudt@gmail.com>
2025-08-15 12:35:18 -0400

    Add files via upload

---
+++ Ordem de Servico/src/jdbc/TesteConexao.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jdbc;
+
+/**
+ * 
+ *
+ * @author GERAL
+ */
+public class TesteConexao {
+    
+    public static void main(String[] args) {
+        
+        try {
+            ModuloConexao.conectar();
+        } catch (Excepition e){
+            
+            JOptionpane.showMessageDialog(null, "Opa aconteceu o erro:" + erro)
+        }
+                
+ 
+    
+    }
+    
+    }
+   
